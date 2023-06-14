4ff53ade462a02533b893f9836ef8248cd8e8fba
ABHISHEK.N <119723609+ABHISHEK-J7@users.noreply.github.com>
2023-06-14 21:05:15 +0530

    #include<stdio.h> int main() {     long int a,b,r,c=0;     scanf("%ld",&a);     b=a;     while(a>0)     {         c++;         a=a/10;     }     if(c==10)     {         if(b/1000000000==0)         {             printf("Invalid");         }         else{             printf("Valid");         }     }     else     {         printf("Invalid");     } }

--- PHONE_NUMBER_VALIDATION:.c
+++ PHONE_NUMBER_VALIDATION:.c
@@ -1,19 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	long long int mno,count=0;
-	scanf("%lld",&mno);
-	while(mno>0)
-	{
-		mno=mno/10;
-		count++;
-	}
-	if(count==10)
-	{
-		printf("Valid");
-	}
-	else
-	{
-		printf("Invalid");
-	}
+    long int a,b,r,c=0;
+    scanf("%ld",&a);
+    b=a;
+    while(a>0)
+    {
+        c++;
+        a=a/10;
+    }
+    if(c==10)
+    {
+        if(b/1000000000==0)
+        {
+            printf("Invalid");
+        }
+        else{
+            printf("Valid");
+        }
+    }
+    else
+    {
+        printf("Invalid");
+    }
 }
