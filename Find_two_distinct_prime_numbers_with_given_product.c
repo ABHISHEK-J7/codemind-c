6ae6d8896512d7aeaf05796117620ff790202d1c
ABHISHEK.N <119723609+ABHISHEK-J7@users.noreply.github.com>
2023-06-17 21:43:20 +0530

    #include<stdio.h> int prime(int num) {     int i;     for (i=2; i*i<=num; i++)     {         if (num%i==0)         {             return 0;         }     }     return 1;     } int main() {     int a;     scanf("%d",&a);     int i,j,c=0;     int p1,p2;     for (i=2; i<=1000; i++)     {         for (j=2; j<=1000; j++)         {             if (i!=j && i*j<=a)             {                 if (prime(i)==1 && prime(j)==1)                 {                     if (i*j==a)                     {                         p1=i;                         p2=j;                         c=1;                         break;                     }                 }             }         }     }     if (c==1)     {         printf("%d %d",p2,p1);     }     else     {         printf("%d",-1);     }     return 0; }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-int prime(int a)
+int prime(int num)
 {
     int i;
-    for (i=2; i*i<=a; i++)
+    for (i=2; i*i<=num; i++)
     {
-        if (a%i==0)
+        if (num%i==0)
         {
             return 0;
         }
@@ -13,33 +13,36 @@ int prime(int a)
     }
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    int i,j,a,b;
-    int c=0;
-    for(i=0;i<n;i++)
+    int a;
+    scanf("%d",&a);
+    int i,j,c=0;
+    int p1,p2;
+    for (i=2; i<=1000; i++)
     {
-        for(j=0;j<n;j++)
+        for (j=2; j<=1000; j++)
         {
-            if(i*j==n && i!=j && prime(i)==1 && prime(j)==1)
+            if (i!=j && i*j<=a)
             {
-                a=i;
-                b=j;
-                c=1;break;
+                if (prime(i)==1 && prime(j)==1)
+                {
+                    if (i*j==a)
+                    {
+                        p1=i;
+                        p2=j;
+                        c=1;
+                        break;
+                    }
+                }
             }
-        }
-        if (c==1)
-        {
-            break;
         }
     }
     if (c==1)
     {
-        printf("%d %d",a,b);
+        printf("%d %d",p2,p1);
     }
     else
     {
-        printf("-1");
+        printf("%d",-1);
     }
     return 0;
 }
