fa56168595359859a9a768bc98e6e816dfd27acb
ABHISHEK.N <119723609+ABHISHEK-J7@users.noreply.github.com>
2023-06-23 20:31:25 +0530

    #include <stdio.h> int main() {     int i, j, rows;     scanf("%d", &rows);     for(i=1; i<=rows; i++)     {         for(j=1; j<=i; j++)         {             if(j==1 || j==i || i==rows)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

---
+++ print_the_following_pattern_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+    scanf("%d", &rows);
+    for(i=1; i<=rows; i++)
+    {
+        for(j=1; j<=i; j++)
+        { 
+            if(j==1 || j==i || i==rows)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+}
